257066ede5c050d0972d5fe4d91d5b7a274067e5
Dan Hogan <dan1ate2@gmail.com>
2017-01-22 11:34:47 +1000

    Trialing npm/bower/gulp setup and getting jquery and bootstrap to minify and output for production

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,10 @@
  
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var mainBowerFiles = require('gulp-main-bower-files');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var gulpFilter = require('gulp-filter');
  
 gulp.task('sass', function () {
   return gulp.src('wp-content/themes/allcarpets/sass/**/*.scss')
@@ -12,3 +16,32 @@ gulp.task('sass', function () {
 gulp.task('sass:watch', function () {
   gulp.watch('wp-content/themes/allcarpets/sass/**/*.scss', ['sass']);
 });
+
+// test-gulp
+gulp.task('main-bower-files', function() {
+    var filterJS = gulpFilter('**/*.js', { restore: true });
+    return gulp.src('bower.json')
+        .pipe(mainBowerFiles({
+            overrides: {
+                bootstrap: {
+                    main: [
+                        './dist/css/*.min.*',
+                        './dist/fonts/*.*'
+                    ]
+                }
+            }
+        }))
+        .pipe(filterJS)
+        .pipe(concat('vendor.js'))
+        .pipe(uglify())
+        .pipe(filterJS.restore)
+        .pipe(gulp.dest('wp-content/themes/allcarpets/test-gulp'));
+});
+
+// test-gulp-2
+// gulp.task('uglify', function(){
+//     return gulp.src('bower.json')
+//         .pipe(mainBowerFiles( ))
+//         .pipe(uglify())
+//         .pipe(gulp.dest('wp-content/themes/allcarpets/test-gulp-2'));
+// });
